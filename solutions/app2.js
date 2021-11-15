b781f12efbf57b45b7900fd5860991fb3f5f4515
Cserhati Imre Andras <cserhati.imre.andras@gmail.com>
2021-11-15 01:01:01 +0100

    completed

--- solutions/app2.js
+++ solutions/app2.js
@@ -1,1 +1,25 @@
+const cookieHandler = {
+  getAll() {
+    const cookies = document.cookie.split('; ');
+    const keyValueCookiesObj = {};
+    cookies.forEach((cookie) => {
+      const keyAndValue = cookie.split('=');
+      keyValueCookiesObj[keyAndValue[0]] = keyAndValue[1];
+    });
+    return keyValueCookiesObj;
+  },
+  toSessionStorage() {
+    const cookies = this.getAll();
+    for (const prop in cookies) {
+      sessionStorage.setItem(prop, cookies[prop]);
+    }
+  },
+  flush() {
+    const cookies = this.getAll();
+    for (const prop in cookies) {
+      document.cookie = `${prop}=${cookies[prop]}; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`;
+    }
+  },
+};
 
+export { cookieHandler };
